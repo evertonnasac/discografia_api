f7a070f92ad9dacf1af1df3757c4cd030996b9f3
Everton <nazare.everton@gmail.com>
2024-08-06 22:15:23 -0300

    Ajustes nos tipos de dados das tabelas

--- database/migrations/2024_07_30_003223_create_faixa_table.php
+++ database/migrations/2024_07_30_003223_create_faixa_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->timestamps();
             $table->string("name");
             $table->string("duration");
-            $table->string("disco_id");
+            $table->integer("disco_id");
         });
     }
 
